5d1b382038fc443b3b412e8f1e27ecefb5453c71
Lisu11 <piotrek.lisowski11@gmail.com>
2026-01-12 10:59:27 +0100

    chore: remove trial licence

--- assets/js/hooks/gridHook.js
+++ assets/js/hooks/gridHook.js
@@ -5,8 +5,6 @@ import {AllEnterpriseModule, LicenseManager} from "ag-grid-enterprise";
 // TODO: select only needed modules
 ModuleRegistry.registerModules([AllCommunityModule, AllEnterpriseModule]);
 
-LicenseManager.setLicenseKey("[TRIAL]_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-117499}_is_granted_for_evaluation_only___Use_in_production_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_purchasing_a_production_key_please_contact_info@ag-grid.com___You_are_granted_a_{Single_Application}_Developer_License_for_one_application_only___All_Front-End_JavaScript_developers_working_on_the_application_would_need_to_be_licensed___This_key_will_deactivate_on_{10 February 2026}____[v3]_[0102]_MTc3MDY4MTYwMDAwMA==a9b79adbe582d613510d294b70e2387d")
-
 function createAgGrid(node) {
     node.grid = createGrid(node.el, {
         theme: themeQuartz.withPart(colorSchemeDarkBlue),
